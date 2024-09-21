0c8905d9608ded3387a7da3a26546c98e5a7f384
Guillaume Clochard <guillaume.clochard@blablacar.com>
2024-09-21 10:45:58 +0200

    update

--- demo.1m.sh
+++ demo.1m.sh
@@ -1,7 +1,54 @@
 #!/usr/bin/env bash
-echo "This is a primary Menu Title"
-echo "This is a secondary Menu Title"
-echo "This is a n-th Menu Title"
-echo "---"
-echo "This is not a Menu Title, this will be shown in the drop-down menu only"
 
+# echo ':tray.and.arrow.down.fill: 1⚑ 0✗/2 | color=#111111 size=11'
+# echo '---'
+# echo '1 notifications | color=#111111 href=http://github.com/notifications'
+# echo '-- vanilla-extract-css/vanilla-extract'
+# echo '--💬 Imports inside .css.ts files produce an error (Next Js) | href=https://github.com/vanilla-extract-css/vanilla-extract/issues/1043 color=#111111'
+# echo '---'
+# echo 'Releases'
+# echo '---'
+# echo 'Pull requests| color=#111111'
+# echo 'blablacar-interview/frontend-livecoding-l1 - ✅ |href=https://github.com/blablacar-interview/frontend-livecoding-l1/pulls color=#111111'
+# echo '⚙️  #4 Add Palindrome exercice | href=https://github.com/blablacar-interview/frontend-livecoding-l1/pull/4 color=#111111'
+# echo '--palindrome | color=#111111 terminal=false bash="/bin/bash" param1="-c" param2="echo -n palindrome | pbcopy"'
+# echo "-- --- | color=#333333"
+# echo '⚙️  #3 Add form exercice | href=https://github.com/blablacar-interview/frontend-livecoding-l1/pull/3 color=#111111'
+# echo '--form | color=#111111 terminal=false bash="/bin/bash" param1="-c" param2="echo -n form | pbcopy"'
+#
+# echo ":tray.and.arrow.down.fill: 1⚑ 0✗/2 | color=#111111 size=11"
+# echo "---"
+# echo "1 notifications | color=#111111 href=http://github.com/notifications"
+# echo "-- vanilla-extract-css/vanilla-extract"
+# echo "--💬 Imports inside .css.ts files produce an error (Next Js) | href=https://github.com/vanilla-extract-css/vanilla-extract/issues/1043 color=#111111"
+# echo "---"
+# echo "Releases"
+# echo "---"
+# echo "Pull requests| color=#111111"
+# echo "blablacar-interview/frontend-livecoding-l1 - ✅ |href=https://github.com/blablacar-interview/frontend-livecoding-l1/pulls color=#111111"
+# echo "#4 Add Palindrome exercice | href=https://github.com/blablacar-interview/frontend-livecoding-l1/pull/4 color=#111111"
+# echo '--palindrome | coloe=#111111 terminal=false bash="/bin/bash" param1="-c" param2="echo -n palindrome | pbcopy"'
+# echo "-- --- | color=#333333"
+# echo "#3 Add form exercice | href=https://github.com/blablacar-interview/frontend-livecoding-l1/pull/3 color=#111111"
+# echo '--form | color=#111111 terminal=false bash="/bin/bash" param1="-c" param2="echo -n form | pbcopy"'
+# echo "-- --- | color=#333333"
+
+
+cat << EOF
+:tray.and.arrow.down.fill: 1⚑ 0✗/2 | color=#111111 size=11
+---
+1 notifications | color=#111111 href=http://github.com/notifications
+-- vanilla-extract-css/vanilla-extract
+--💬 Imports inside .css.ts files produce an error (Next Js) | href=https://github.com/vanilla-extract-css/vanilla-extract/issues/1043 color=#111111
+---
+Releases
+---
+Pull requests| color=#111111
+blablacar-interview/frontend-livecoding-l1 - ✅ |href=https://github.com/blablacar-interview/frontend-livecoding-l1/pulls color=#111111
+⚙️  #4 Add Palindrome exercice | href=https://github.com/blablacar-interview/frontend-livecoding-l1/pull/4 color=#111111
+--palindrome | color=#111111 terminal=false bash="/bin/bash" param1="-c" param2="echo -n palindrome | pbcopy"
+-- --- | color=#333333
+⚙️  #3 Add form exercice | href=https://github.com/blablacar-interview/frontend-livecoding-l1/pull/3 color=#111111
+--form | color=#111111 terminal=false bash="/bin/bash" param1="-c" param2="echo -n form | pbcopy"
+-- --- | color=#333333
+EOF
